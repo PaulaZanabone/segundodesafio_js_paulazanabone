57a8fe28b2423c1942c1a98d2cc081f5bb9aa968
Paula Zanabone <pzanabone1@gmail.com>
2022-05-25 23:02:05 -0300

    Primer desafío entregable JAVASCRIPT Paula Zanabone

---
+++ JAVASCRIPT/archivosjs.js
@@ -0,0 +1,41 @@
+//let nombreCompleto= prompt("Suscribite para recibir ofertas")
+//alert("quiero recibir ofertas")
+//confirm("¿desea suscribirse para recibir nuestras ofertas?")
+
+
+
+//let username = prompt("Ingrese su nombre:")
+//let username = "atodos"
+//if(username == "atodos"){
+//console.log("Bienvenidos, username")
+//} else{
+//    console.warn ("No se reconoce el usuario ingresado")
+//}
+
+//let suscripcion = prompt ("quiero recibir ofertas")
+//suscripcion=suscripcion
+
+debugger
+
+//let respuesta = confirm ("¿Está seguro que se quiere subscribir?")
+//if(respuesta == true) {
+//console.warn("Ingrese su correo electrónico")
+//}else{
+ //   console.log("Seguí navegando en Las TOTAS")
+//}
+
+
+let limite = parseInt(prompt("Ingrese un correo electronico:"))
+for (let i = 0; i < limite; i++) {
+        if (i === 5) {
+            continue
+        }
+        console.log("Correo válido:", i)
+    }
+
+    let seguimos = true
+
+ while (seguimos) {
+     console.log("Correo invalido.")
+     seguimos = confirm("¿Olvidaste la contraseña?")
+ }
