796f029d7e7af9a10f4203a58be66d4d33ae13ed
Paula Zanabone <pzanabone1@gmail.com>
2022-06-02 18:30:17 -0300

    segunda entrega paula zanabone

---
+++ JAVASCRIPT/funcion.js
@@ -0,0 +1,13 @@
+function calculadora (){
+    debugger
+let primerNro = prompt("Ingrese el producto al carrito")  
+let segundoNro = prompt("Ingrese el otro producto al carrito")
+realizarCalculo = (primerNro, segundoNro) 
+}
+
+function realizarCalculo (primerNro, segundoNro){
+    let resultado = parseInt (primerNro) + parseInt (sengundoNro)
+    console.log("Resultado", resultado)
+}
+
+calculadora()
